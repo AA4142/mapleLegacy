c595de194c184049380ea08d90c2c6108da7c151
AA4142 <alexarmatas5@gmail.com>
2022-11-20 02:45:06 -0500

    created basic movement

--- maple.js
+++ maple.js
@@ -6,15 +6,38 @@ const playerSprite = document.getElementById("daGoat");
 
 context.fillStyle = "blue";
 context.fillRect(0,0, 1280, 720);
-context.drawImage(playerSprite, 50, 50);
 
+/*
+
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 5;
 
 function animate(){
     context.clearRect(0, 0, 1280, 720);
+    context.drawImage(playerSprite, 50, 50);
+    if (gameFrame % staggerFrames == 0){
+
+    }
+
+    gameFrame++;
+    requestAnimationFrame(animate);
     
+};
+animate();
+*/
+
+const player = {
+    height: 64,
+    jumping: true,
+    width: 39,
+    x: 0,
+    xVelocity:0,
+    y:0,
+    yVelocity: 0
 }
 
-/*
 const controller = {
     left: false,
     right: false,
@@ -36,28 +59,56 @@ const controller = {
         }
     }
 
-}
+};
 
 const loop = function(){
     if(controller.up && player.jumping == false){
-        player.yVelocity -= 20;
-        square.jumping = true;
+        player.yVelocity -= 15;
+        player.jumping = true;
     }
 
     if(controller.left){
-        player.xVelocity -= 0.5;
+        player.xVelocity -= 0.4;
     }
 
     if(controller.right){
-        player.xVelocity += 0.5;
+        player.xVelocity += 0.4;
     }
 
-    player.yVelocity += 1.5;
+    player.yVelocity += 1;
     player.x += player.xVelocity;
     player.y += player.yVelocity;
     player.xVelocity *= 0.9;
     player.yVeloicty *= 0.9;
-}
 
-*/
-//the loop function should get values from a map that set borders etc
+    if(player.y > 720 - 64 - 32){
+
+        player.jumping = false;
+        player.y =  720 - 64- 32;
+        player.yVelocity = 0;
+    }
+
+    if(player.x < 0){
+        player.x = 0;
+    }else if(player.x > 1180){
+        player.x = 1180;
+    }
+
+    //draw background
+    context.fillStyle="blue";
+    context.fillRect(0, 0, 1280, 720);
+
+    //draw player
+    context.drawImage(playerSprite, player.x, player.y);
+
+    window.requestAnimationFrame(loop);
+
+
+
+
+};
+
+window.addEventListener("keydown", controller.keyListener);
+window.addEventListener("keyup", controller.keyListener);
+window.requestAnimationFrame(loop);
+
